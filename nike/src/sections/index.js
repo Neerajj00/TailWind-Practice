f451f200bc5d2a015164eb204cc07b93d1c9f2e4
Neeraj-gupta2005 <neerajKgupta33@gmail.com>
2024-09-09 22:36:23 +0530

    first commit

---
+++ nike/src/sections/index.js
@@ -0,0 +1,21 @@
+import Hero from "./hero";
+import PopularProducts from "./PopularProduct";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffers";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import Footer from "./footer";
+import Nav from "../Components/Nav";    // Add this line
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffers,
+    CustomerReviews,
+    Subscribe,
+    Footer,
+    Nav
+}
